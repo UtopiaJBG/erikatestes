ccd30e37af6c701a0d05fa039bf92139873b84cb
João Bastos Garrido <151746625+UtopiaJBG@users.noreply.github.com>
2024-03-13 16:54:07 -0300

    Update teste1.py

--- Erika/teste1.py
+++ Erika/teste1.py
@@ -162,7 +162,7 @@ def main():
     "Quantia Atual"]
         medicamentos_filtrados_editar["Data de Validade"] = pd.to_datetime(medicamentos_filtrados_editar["Data de Validade"])
         medicamentos_filtrados_editar = medicamentos_filtrados_editar.sort_values(by=["Data de Validade"])
-
+        
         # Converte as datas para o formato esperado pelo pandas
         medicamentos_filtrados_editar["Data de Validade"] = medicamentos_filtrados_editar["Data de Validade"].dt.strftime('%d/%m/%Y')
         st.dataframe(medicamentos_filtrados_editar[columns_to_display].assign(**{"Data de Validade": medicamentos_filtrados_editar["Data de Validade"].dt.strftime('%d/%m/%Y')}), height=600)
